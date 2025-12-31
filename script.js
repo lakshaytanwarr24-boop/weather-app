1e85da70f5e28a1844dd3cb67584a2784ee2d013
Lakshay <234445196+lakshaytanwarr24-boop@users.noreply.github.com>
2026-01-01 00:05:32 +0530

    Update script.js
    Added OpenWeather API key

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getWeather() {
   const city = document.getElementById("city").value;
-  const apiKey = "YOUR_API_KEY";
+  const apiKey = "82c9607994c09bf765b0e8bf3c79f9b1";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
   fetch(url)
